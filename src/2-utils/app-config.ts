ff06f0e734fcf0df38b791a32689f15f046f99b2
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-07-21 11:11:12 +0300

    appConfig and package.json changes for heroku

--- src/2-utils/app-config.ts
+++ src/2-utils/app-config.ts
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class AppConfig {
-  public readonly isDevelopment = process.env.ENVIRONMENT === "development";
-  public readonly isProduction = process.env.ENVIRONMENT === "production";
+  public readonly isDevelopment = process.env.NODE_ENV === "development";
+  public readonly isProduction = process.env.NODE_ENV === "production";
   public readonly port = process.env.PORT;
   public readonly mongodbConnectionString =
     process.env.MONGODB_CONNECTION_STRING;
